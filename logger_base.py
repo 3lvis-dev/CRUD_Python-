96922e47c3db13b32aa5ed86da326f00ad88e629
Elvis-Aef <ea.espinoza.f@gmail.com>
2021-07-05 11:20:07 -0400

    Inicio del proyecto CRUD

---
+++ logger_base.py
@@ -0,0 +1,17 @@
+#Capa de Datos.
+#Manejo de Logging.
+
+'''
+Nota: pagina documentación: docs.python.org/3/howto/logging.html
+'''
+import psycopg2 as bd
+import logging as log
+
+log.basicConfig(level=log.DEBUG)
+
+if __name__ == '__main__':
+	log.debug('Mensaje a nivel debug')
+	log.info('Mensaje a nivel info')
+	log.warning('Mensaje a nivel warning')
+	log.error('Mensaje a nivel error')
+	log.critical('Mensaje a nivel critico')
